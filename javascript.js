57211ba7aaff9f4bf4ed261046ac477166be26de
Marko Kask <kaskmarko@gmail.com>
2022-11-09 21:10:32 +0200

    Add basic drawing functionality

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const drawingBoard = document.querySelector(".drawingBoard");
+let grid = false;
 
 function fillDrawingBoard(boardSize){
     let squares = drawingBoard.querySelectorAll("div");
@@ -11,13 +12,39 @@ function fillDrawingBoard(boardSize){
     for(let i = 0; i < (boardSize * boardSize); i++){
         let square = document.createElement("div");
         drawingBoard.appendChild(square);
+        if(grid){
+            square.style.border = "1px solid grey";
+        }
+        // mousemove
+        square.addEventListener("mousedown",()=>{
+            if(isDrawing){
+                square.style.background = "black";
+            }
+        });
+        square.addEventListener("mouseover", ()=>{
+            if(isDrawing){
+                square.style.background = "black";
+            }
+        });
+        square.addEventListener("click", ()=>{
+            square.style.background = "black";
+        });
     }   
 }
+
+let isDrawing = false;
+window.addEventListener("mousedown", ()=>{
+    isDrawing = true;
+});
+
+window.addEventListener("mouseup", ()=>{
+    isDrawing = false;
+});
+
 
 fillDrawingBoard(16);
 
 /* Slider to change number of squares on drawing board*/
-
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value; 
@@ -28,7 +55,7 @@ slider.oninput = function() {
 }
 
 
-/* Show Grid*/
+/* Show/Disable Grid*/
 function gridOnOff(){    
     let gridButton = document.querySelector("#btn-show-grid");
     let currentValue = document.getElementById("btn-show-grid").value;
@@ -40,12 +67,14 @@ function gridOnOff(){
             div.style.border = "1px solid grey";   
         });
         gridButton.innerHTML = "Disable grid";
+        grid = true;
     }else{
         document.getElementById("btn-show-grid").value="gridOff";
         squares.forEach((div) => {
             div.style.border = "none";   
         });
         gridButton.innerHTML = "Show grid";
+        grid = false;
     }
 }
 
